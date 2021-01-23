17458daec32d183d12cc4739c985820c694cf6b7
Keisuke Kuzukawa <kuzukawa@gmail.com>
2021-01-23 17:19:40 +0900

    add my contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,24 @@
 module.exports = {
   siteMetadata: {
     title: "My Gatsby Site",
+    description: "Hello Gatsuby",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-rss-feed`,
+      options: {
+        url: `https://qiita.com/kkuzu/feed`,
+        name: `Qiita`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:{
+        name: `works`,
+        path: `${__dirname}/src/works.yaml`,
+      },
+    },
+    `gatsby-transformer-yaml`,
+    `gatsby-plugin-react-helmet`
+  ],
 };
